ecf9e81cfc0ca8748b33c2e51ed96813cf447162
gbrreel <gabrielggalloo@gmail.com>
2025-01-18 19:26:31 -0300

    Iniciando o desenvolvimento

---
+++ CalculadoraCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace Calculadora{
+    internal class Program{
+        static void Main(string[] args){
+            System.Console.WriteLine("Digite o primeiro número: ");
+        }
+    }
+}
