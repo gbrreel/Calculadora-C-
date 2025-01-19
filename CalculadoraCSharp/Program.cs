54405d04d33a6bd2eb01e5dbaa23d43d1a1687b1
gbrreel <gabrielggalloo@gmail.com>
2025-01-19 10:48:03 -0300

    Adicionando primeiros comandos

--- CalculadoraCSharp/Program.cs
+++ CalculadoraCSharp/Program.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 namespace Calculadora{
     internal class Program{
         static void Main(string[] args){
-            System.Console.WriteLine("Digite o primeiro número: ");
+
+            Console.WriteLine("Qual operação deseja realizar? ");
+            Console.WriteLine("1- Adicao");
+            Console.WriteLine("2- Subtracao");
+            Console.WriteLine("3- Multiplicacao");
+            Console.WriteLine("4- Divisao \n");
+
+            int operacao = int.Parse(Console.ReadLine());
+
+
+
+
         }
     }
 }
