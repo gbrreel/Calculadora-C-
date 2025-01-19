7ac3e1a0926bc444075c1df9532d367c8bf384d8
gbrreel <gabrielggalloo@gmail.com>
2025-01-19 10:58:35 -0300

    Teste IDE novamente

--- CalculadoraCSharp/Program.cs
+++ CalculadoraCSharp/Program.cs
@@ -8,13 +8,10 @@ namespace Calculadora{
     internal class Program{
         static void Main(string[] args){
 
-            Console.WriteLine("Qual operação deseja realizar? ");
-            Console.WriteLine("1- Adicao");
-            Console.WriteLine("2- Subtracao");
-            Console.WriteLine("3- Multiplicacao");
-            Console.WriteLine("4- Divisao \n");
+
 
             int operacao = int.Parse(Console.ReadLine());
+
 
 
 
